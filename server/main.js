80c0cf8cb025489fa1fd34e51d6b5178cca17fb8
centrino.kib@gmail.com <centrino.kib@gmail.com>
2022-11-12 13:31:06 +0900

    Feat: Added google vision & translation api librararies. Set up server directory

---
+++ server/main.js
@@ -0,0 +1,9 @@
+// Imports the Google Cloud client library
+const textToSpeech = require("@google-cloud/text-to-speech");
+const translate = require("@google-cloud/translate").v3beta1;
+const vision = require("@google-cloud/vision");
+
+// Import other required libraries
+const fs = require("fs");
+//const escape = require('escape-html');
+const util = require("util");
