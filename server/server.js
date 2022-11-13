ba74a5caae1833623855c0e7784da1ac8a668dc2
centrino.kib@gmail.com <centrino.kib@gmail.com>
2022-11-13 12:30:16 +0900

    Feat: Added endpoints for testing, return of user tables and posting new user

--- server/server.js
+++ server/server.js
@@ -1,10 +1,37 @@
 const express = require("express");
 const path = require("path");
+const db = require("./knex");
 
 function setupServer() {
 	const app = express();
 
 	app.use(express.static(path.resolve(__dirname, "../client/build")));
+	app.use(express.json());
+
+	app.get("/test", async (res, req) => {
+		res.status(200).send("successful get");
+	});
+
+	app.get("/users", async (res, req) => {
+		// console.log(req.body);
+		try {
+			const users = await db("users_table").select("*");
+			res.status(200).send(users);
+		} catch (error) {
+			res.status(500).send(error);
+		}
+	});
+
+	app.post("/new_user", async (res, req) => {
+		try {
+			const payload = req.body;
+			await db("users_table").insert(payload);
+
+			res.status(200).send("Success");
+		} catch (error) {
+			res.status(500).send(error);
+		}
+	});
 
 	return app;
 }
