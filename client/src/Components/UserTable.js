07f8ca9a2f392a1c0546e06237a5f90b108d621c
centrino.kib@gmail.com <centrino.kib@gmail.com>
2022-11-14 13:57:01 +0900

    Fix: axios error for delete request resolved

--- client/src/Components/UserTable.js
+++ client/src/Components/UserTable.js
@@ -4,44 +4,32 @@ import axios from "axios";
 export default function UsersTable() {
 	const [usersTable, setUserTable] = useState([]);
 	const [Name, setName] = useState("");
-	const [temp, setTemp] = useState("no");
 
 	useEffect(() => {
 		getUsers();
-	}, []);
+	}, [usersTable]);
 
 	async function getUsers() {
 		try {
 			const fetchUsers = await axios.get("/users");
 			setUserTable(fetchUsers.data);
-			setTemp("yes");
 		} catch (error) {
 			console.log(error);
 		}
 	}
 
-	// async function removeUser(id) {
-	// 	try {
-	// 		await axios.delete(`/users/${id}`);
-	// 	} catch (error) {
-	// 		console.log(error);
-	// 	}
-	// }
-
 	async function removeUser(id) {
-		// e.preventDefault();
 		await axios.delete(`/users/${id}`);
-		console.log(id);
+		setUserTable(usersTable.filter((el) => el.id !== id));
 	}
 
-	// if (temp === "yes") {
 	const usersList = usersTable.map((user) => {
 		const fullName = user.first_name + " " + user.last_name;
 		return (
 			<p>
 				<span>
 					{fullName} {user.age}
-					<button onClick={removeUser}>Remove</button>
+					<button onClick={() => removeUser(user.id)}>Remove</button>
 					<button>Edit</button>
 				</span>
 			</p>
