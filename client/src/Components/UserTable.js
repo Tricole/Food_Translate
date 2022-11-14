ea1ea1442b89f82a45144c3ab22aad7b815bec01
centrino.kib@gmail.com <centrino.kib@gmail.com>
2022-11-14 16:30:15 +0900

    Update: Edit feature for user now completed

--- client/src/Components/UserTable.js
+++ client/src/Components/UserTable.js
@@ -21,18 +21,34 @@ export default function UsersTable() {
 
 	async function updateUser(event, user) {
 		event.preventDefault(event);
-		console.log(user);
-		console.log(event.target.first_name.value);
+		// console.log(user);
+		// console.log(event.target.first_name.value);
+		console.log(user.diet_restrictions);
 
-		// const age = event.target.age.value? event.target.age.value :
+		const age = event.target.age.value ? event.target.age.value : user.age;
+		const first_name = event.target.first_name.value
+			? event.target.first_name.value
+			: user.first_name;
+		const last_name = event.target.last_name.value
+			? event.target.last_name.value
+			: user.last_name;
+		const height = event.target.height.value
+			? event.target.height.value
+			: user.height;
+		const activity = event.target.activity.value
+			? event.target.activity.value
+			: user.activity;
+		const diet_restrictions = event.target.diet_restrictions.value
+			? event.target.diet_restrictions.value
+			: user.diet_restrictions;
 
 		const updateUser = {
-			first_name: `${event.target.first_name.value}`,
-			last_name: `${event.target.last_name.value}`,
-			age: `${event.target.age.value}`,
-			height: `${event.target.height.value}`,
-			activity: `${event.target.activity.value}`,
-			diet_restrictions: `${event.target.diet_restrictions.value}`,
+			first_name: first_name,
+			last_name: last_name,
+			age: age,
+			height: height,
+			activity: activity,
+			diet_restrictions: JSON.stringify(diet_restrictions.split(" ")),
 		};
 
 		// const updatedUser = {
